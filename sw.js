c80a07735ff98da1d7228873f04f6b2ad7019a6a
عبدالله حسين المطيري <abdullaha0569361@gmail.com>
2026-09-23 00:43:05 +0300

    Add service worker install and fetch event listeners

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+});
 
+self.addEventListener('fetch', (event) => {
+  // يمكنك إضافة كود التخزين المؤقت هنا لاحقاً
+});
